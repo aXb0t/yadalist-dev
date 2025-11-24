9423b9bd31c6d71d8b816453971f814a105d295e
alex <alex@macbookpro.lan>
2025-11-24 16:35:29 -0700

    Hope this points to static

--- schmango/settings.py
+++ schmango/settings.py
@@ -105,10 +105,10 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-# Additional static file locations (for development)
-STATICFILES_DIRS = [
-    BASE_DIR / "frontend" / "dist",
-]
+# Additional static file locations (for development only)
+STATICFILES_DIRS = []
+if DEBUG and (BASE_DIR / "frontend" / "dist").exists():
+    STATICFILES_DIRS = [BASE_DIR / "frontend" / "dist"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
