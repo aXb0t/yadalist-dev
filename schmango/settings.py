b07eb8b7da7ef9b5a3e1ba1a206e09340a7c87b2
alex <alex@macbookpro.lan>
2025-11-24 06:51:06 -0700

    NGINX trusts ssl headers

--- schmango/settings.py
+++ schmango/settings.py
@@ -13,6 +13,9 @@ DEBUG = os.environ.get("DEBUG", "True") == "True"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 # Security settings
+# Trust the X-Forwarded-Proto header from nginx proxy
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
 SECURE_SSL_REDIRECT = os.environ.get("SECURE_SSL_REDIRECT", "False") == "True"
 SESSION_COOKIE_SECURE = os.environ.get("SESSION_COOKIE_SECURE", "False") == "True"
 CSRF_COOKIE_SECURE = os.environ.get("CSRF_COOKIE_SECURE", "False") == "True"
