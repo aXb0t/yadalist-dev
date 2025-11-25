0f1ec3d12be890a9e361ab3b7a6568876617b4a6
alex <alex@macbookpro.lan>
2025-11-25 11:42:24 -0700

    Split settings files per env

--- schmango/asgi.py
+++ schmango/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'schmango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'schmango.settings.production')
 
 application = get_asgi_application()
